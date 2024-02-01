d9af4ac3eb005b348b78cc6ecf7151c367f8427a
xp2000926 <3053949745@qq.com>
2024-02-01 22:31:52 +0800

    feat: 完善功能

--- src/command/create.ts
+++ src/command/create.ts
@@ -1,8 +1,11 @@
 import { select, input } from "@inquirer/prompts";
 import { clone } from "../utils/clone";
+import { name, version } from "../../package.json";
 import path from "path";
-import fs from "fs-extra";
+import fs, { lchmod } from "fs-extra";
 import chalk from "chalk";
+import axios, { AxiosResponse } from "axios";
+import { gt } from "lodash";
 
 export interface TemplateInfo {
   name: string; // 项目名称
@@ -49,6 +52,39 @@ export async function isOverwrite(fileName: string) {
     ],
   });
 }
+export const getNpmInfo = async (npmName: string) => {
+  const npmUrl = `https://registry.npmjs.org/${npmName}`;
+  let res = {};
+  try {
+    res = await axios.get(npmUrl);
+  } catch (error) {
+    console.log(chalk.red(error));
+  }
+  return res;
+};
+export const getNpmLatestVersion = async (npmName: string) => {
+  const { data } = (await getNpmInfo(npmName)) as AxiosResponse;
+  return data["dist-tags"].latest;
+};
+export const checkVersion = async (name: string, version: string) => {
+  const latestVersion = await getNpmLatestVersion(name);
+  const need = gt(latestVersion, version);
+  if (need) {
+    console.log(
+      chalk.yellow(
+        `检测到 create-vite-vue-ts 最新版:${chalk.blueBright(
+          latestVersion
+        )} 当前版本:${chalk.blueBright(version)}`
+      )
+    );
+    console.log(
+      `可使用:${chalk.yellow(
+        "npm install create-vite-vue-ts-cli@latest -g"
+      )} 或者使用:${chalk.yellow("create-vite-vue-ts update")}更新`
+    );
+  }
+  return need;
+};
 export async function create(projectName: string) {
   // 初始模板列表
   const templateList = Array.from(templates).map(
@@ -72,6 +108,8 @@ export async function create(projectName: string) {
       return; // 不覆盖直接结束
     }
   }
+  // 监测版本更新
+  await checkVersion(name, version);
 
   const templateName = await select({
     message: "请选择一个模板",
